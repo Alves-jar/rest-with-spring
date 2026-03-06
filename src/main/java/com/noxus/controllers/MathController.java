d8c259cc67369dea3894babe1602e2f3bfcdb08a
AlvesKZ <igoralvex01@gmail.com>
2026-03-06 14:44:20 -0300

    feat: create math controller

---
+++ src/main/java/com/noxus/controllers/MathController.java
@@ -0,0 +1,31 @@
+package com.noxus.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) throw new IllegalArgumentException();
+        String number = strNumber.replaceAll(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
