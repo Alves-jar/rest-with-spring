fda9f3489ce85f8fcc39d562d6a92ffa22c184f0
AlvesKZ <igoralvex01@gmail.com>
2026-03-26 12:26:24 -0300

    feat: add method to export csv and xslx files in person service

--- src/main/java/com/noxus/services/PersonServices.java
+++ src/main/java/com/noxus/services/PersonServices.java
@@ -9,6 +9,9 @@ import com.noxus.exception.ResourceNotFoundException;
 
 import static com.noxus.mapper.ObjectMapper.parseObject;
 
+import com.noxus.file.exporter.MediaTypes;
+import com.noxus.file.exporter.contract.FileExporter;
+import com.noxus.file.exporter.factory.FileExporterFactory;
 import com.noxus.file.importer.contract.FileImporter;
 import com.noxus.file.importer.factory.FileImporterFactory;
 import com.noxus.model.Person;
@@ -21,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
@@ -47,6 +51,9 @@ public class PersonServices {
     FileImporterFactory importer;
 
     @Autowired
+    FileExporterFactory exporter;
+
+    @Autowired
     PagedResourcesAssembler<PersonDTO> assembler;
 
     public PagedModel<EntityModel<PersonDTO>> findAll(Pageable pageable) {
@@ -71,6 +78,20 @@ public class PersonServices {
         var dto = parseObject(entity, PersonDTO.class);
         addHateoasLinks(dto);
         return dto;
+    }
+
+    public Resource exportPage(Pageable pageable, String acceptHeader) {
+        logger.info("Exporting a people page!");
+
+        var people = repository.findAll(pageable).map(person -> parseObject(person, PersonDTO.class))
+            .getContent();
+
+        try {
+            FileExporter exporter = this.exporter.getExporter(acceptHeader);
+            return exporter.exportFile(people);
+        } catch (Exception e) {
+            throw new RuntimeException("Error during file export", e);
+        }
     }
 
     public PersonDTO create(PersonDTO person) {
@@ -178,5 +199,6 @@ public class PersonServices {
         dto.add(linkTo(methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
         dto.add(linkTo(methodOn(PersonController.class).disablePerson(dto.getId())).withRel("disable").withType("PATCH"));
         dto.add(linkTo(methodOn(PersonController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+        dto.add(linkTo(methodOn(PersonController.class).exportPage(0, 12, "asc", MediaTypes.APPLICATION_XLSX_VALUE)).withRel("exportPage").withType("GET"));
     }
 }
